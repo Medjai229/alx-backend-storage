b2fa2016a03a3e0aa2ccb3e61634d1ac48d6b0da
Malik <malikhussein229@gmail.com>
2024-08-08 20:14:35 +0300

    Task#10 Safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- Safe division function to avoid division by zero errors.
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+DECLARE result FLOAT DEFAULT 0;
+IF b != 0 THEN
+	SET result = (a / b);
+END IF;
+RETURN result;
+END$$
+
+DELIMITER ;
