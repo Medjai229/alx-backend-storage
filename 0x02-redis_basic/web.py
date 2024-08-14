075f5f782ba8677cb3bba991b7d32b7cf8b20db4
Malik <malikhussein229@gmail.com>
2024-08-14 23:56:29 +0300

    Task#5 Error because the url tested is down

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
-'''
-Caching request module
-'''
-
+""" A python script that interacts with redis database """
 import redis
 import requests
-from functools import wraps
 from typing import Callable
+from functools import wraps
 
 
-def track_get_page(fn: Callable) -> Callable:
-    ''' Decorator for get_page '''
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        ''' wrapper function '''
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+r = redis.Redis()
 
 
-@track_get_page
+def cache_with_count(method: Callable) -> Callable:
+    """
+    A decorator that caches the result of a given method and counts
+    the number of times the result is retrieved.
+    """
+
+    @wraps(method)
+    def invoker(url) -> str:
+        """
+        A wrapper function that caches the result of a given method
+        and counts the number of times the result is retrieved.
+        """
+
+        r.incr(f'count:{url}')
+        result = r.get(f'result:{url}')
+
+        if result:
+            return result.decode('UTF-8')
+
+        result = method(url)
+        r.set(f'count:{url}', 0)
+        r.setex(f'result:{url}', 10, result)
+
+        return result
+
+    return invoker
+
+
+@cache_with_count
 def get_page(url: str) -> str:
-    ''' Function to get page '''
-    response = requests.get(url)
-    return response.text
+    """
+    Retrieves the content of a webpage at a given URL.
+    """
+
+    return requests.get(url).text
